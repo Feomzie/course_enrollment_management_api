8732eafef41ff9a50d17d9eecef292ccd19d6f9e
Feomzie <joyogboh@gmail.com>
2026-02-18 20:00:46 +0100

    feat: set up course and enrollment route

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
-from app.routes.user_route import router
+
+from app.routes.user_route import router as user_router
+from app.routes.course_route import router as course_router
+from app.routes.enrollment_route import router as enrollment_router
+
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Course Enrollment Management")
 
-app.include_router(router)
+app.include_router(user_router)
+app.include_router(course_router)
+app.include_router(enrollment_router)
 
 app.add_middleware(
     CORSMiddleware,
